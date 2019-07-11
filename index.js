95a82dbf52e32bcf7a0444f86ec07ed1ad653237
Feross Aboukhadijeh <feross@feross.org>
2019-07-11 15:19:07 -0700

    standard

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function findInterfacesDarwin (targets) {
     }
 
     for (let j = 0; j < targets.length; j++) {
-      let target = targets[j]
+      const target = targets[j]
       if (target === port.toLowerCase() || target === device.toLowerCase()) {
         interfaces.push(it)
         break
@@ -206,7 +206,7 @@ function findInterfacesWin32 (targets) {
         device: ''
       }
 
-      let result = /adapter (.+?):/.exec(lines[ i ])
+      const result = /adapter (.+?):/.exec(lines[i])
       if (!result) {
         continue
       }
@@ -404,14 +404,14 @@ function randomize (localAdmin) {
   // decrease chance of colliding with existing device's addresses.
 
   const vendors = [
-    [ 0x00, 0x05, 0x69 ], // VMware
-    [ 0x00, 0x50, 0x56 ], // VMware
-    [ 0x00, 0x0C, 0x29 ], // VMware
-    [ 0x00, 0x16, 0x3E ], // Xen
-    [ 0x00, 0x03, 0xFF ], // Microsoft Hyper-V, Virtual Server, Virtual PC
-    [ 0x00, 0x1C, 0x42 ], // Parallels
-    [ 0x00, 0x0F, 0x4B ], // Virtual Iron 4
-    [ 0x08, 0x00, 0x27 ] // Sun Virtual Box
+    [0x00, 0x05, 0x69], // VMware
+    [0x00, 0x50, 0x56], // VMware
+    [0x00, 0x0C, 0x29], // VMware
+    [0x00, 0x16, 0x3E], // Xen
+    [0x00, 0x03, 0xFF], // Microsoft Hyper-V, Virtual Server, Virtual PC
+    [0x00, 0x1C, 0x42], // Parallels
+    [0x00, 0x0F, 0x4B], // Virtual Iron 4
+    [0x08, 0x00, 0x27] // Sun Virtual Box
   ]
 
   // Windows needs specific prefixes sometimes
